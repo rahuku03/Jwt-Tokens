44d5b953923bb4d0ab898aa862b340ef042154d5
rahuku03 <58826161+rahuku03@users.noreply.github.com>
2020-08-06 15:59:59 +0530

    Update UserController.java

--- src/main/java/murraco/controller/UserController.java
+++ src/main/java/murraco/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -20,6 +19,8 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
+import murraco.dto.AuthenticationToken;
+import murraco.dto.UserAuthentication;
 import murraco.dto.UserDataDTO;
 import murraco.dto.UserResponseDTO;
 import murraco.model.User;
@@ -41,10 +42,17 @@ public class UserController {
   @ApiResponses(value = {//
       @ApiResponse(code = 400, message = "Something went wrong"), //
       @ApiResponse(code = 422, message = "Invalid username/password supplied")})
-  public String login(//
-      @ApiParam("Username") @RequestParam String username, //
-      @ApiParam("Password") @RequestParam String password) {
-    return userService.signin(username, password);
+  public AuthenticationToken login(//
+     /* @ApiParam("Username") @RequestParam String username, //
+      @ApiParam("Password") @RequestParam String password)*/
+		  
+		  @RequestBody UserAuthentication user){
+	  
+	  AuthenticationToken token=new AuthenticationToken();
+	  token.setToken(userService.signin(user.getUsername(), user.getPassword()));
+	  
+	  return token;
+   // return userService.signin(username, password);
   }
 
   @PostMapping("/signup")
